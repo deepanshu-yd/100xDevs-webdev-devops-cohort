71960b8d59a3285800df512577589df87b5542de
Deepanshu Yadav <yadavdeepanshu000@gmail.com>
2024-12-25 15:19:09 +0000

    Learning Varibles

---
+++ week-1.2/variables.js
@@ -0,0 +1,5 @@
+const favColor = "red";
+var heightInCentimeters = 180;
+let likePizza = true;
+
+console.log("I like the color " + favColor + ". My height is " + heightInCentimeters + ". " + likePizza + " I like pizza");
