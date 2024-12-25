8d5c866fa15ec8f8007a9e2dc5c2072e4ca03c5d
Deepanshu Yadav <yadavdeepanshu000@gmail.com>
2024-12-25 15:23:57 +0000

    Added datatypes and operators

---
+++ week-1.2/1-variables.js
@@ -0,0 +1,20 @@
+// 1. Variables
+
+const favColor = "red";
+var heightInCentimeters = 180;
+let likePizza = true;
+
+console.log("I like the color " + favColor + ". My height is " + heightInCentimeters + ". " + likePizza + " I like pizza");
+
+/* 2. Datatypes 
+let number = 42;
+let string = "Deepanshu";
+let isBoolean = true;
+let isArray = [1, 2, 3];
+
+
+3. Operators
+
+let sum = 10 + 5;
+let isEqual (10 === 10);
+*/
--- week-1.2/variables.js
+++
@@ -1,5 +0,0 @@
-const favColor = "red";
-var heightInCentimeters = 180;
-let likePizza = true;
-
-console.log("I like the color " + favColor + ". My height is " + heightInCentimeters + ". " + likePizza + " I like pizza");
