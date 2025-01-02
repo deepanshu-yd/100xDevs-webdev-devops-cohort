5729109ee805bd91866701f98597c83fe1d699eb
Your Name <yadavdeepanshu000@gmail.com>
2025-01-02 09:30:36 +0530

    if/else

---
+++ week-1/Day-2/3-if,else-assignment.js
@@ -0,0 +1,9 @@
+// checks the number is even or odd. the print number is even/odd
+
+let num = 10;
+if (num %2 == 0) {
+	console.log("Number is even");	
+}
+else {
+	console.log("Number is odd");	
+}
