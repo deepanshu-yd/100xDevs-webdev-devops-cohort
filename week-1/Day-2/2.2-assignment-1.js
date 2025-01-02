6c958394b2b4be5f195be8032bc8f89e434336e2
Your Name <yadavdeepanshu000@gmail.com>
2025-01-02 09:24:41 +0530

    assignment 2

---
+++ week-1/Day-2/2.2-assignment-1.js
@@ -0,0 +1,11 @@
+// function called canVote that returns true/false if age > 18
+
+function canVote(age) {
+	if (age > 18) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+console.log(canVote(21));
