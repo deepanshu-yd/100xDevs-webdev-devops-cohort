eead043ef41ca1070ac32e6a251366ac6f98f3f3
Your Name <yadavdeepanshu000@gmail.com>
2025-01-06 16:21:41 +0530

    some code

--- Week 2/1 Async JS/2-require-practice.js
+++ Week 2/1 Async JS/2-require-practice.js
@@ -1,9 +1,9 @@
-const fs = require ("fs");
+/* const fs = require ("fs");
 
 const contents = fs.readFileSync("a.txt" , "utf-8");
 const contents2 = fs.readFileSync("b.txt", "utf-8");
 console.log(contents);
-console.log(contents2);
+console.log(contents2); */
 
 // fs is an external library --> Read more about fs (file system) in node js
 // line 4 can be written in 2 different ways
@@ -12,3 +12,16 @@ console.log(contents2);
 
 // line 4 is completely an I/O bound task
 // CPU bound tasks are operations that only runs on the CPU
+
+
+//------------------------------------------------------------------------------------
+
+function timeout () {
+	console.log("Click the button!");
+}
+
+console.log("hi");
+
+setTimeout (timeout, 15000); // this setTimeout function is already present globally in js, so no need to require it
+
+console.log("Welcome to loupe");
