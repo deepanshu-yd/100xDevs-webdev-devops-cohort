90fdd1d38f44e118fb859cae77174dfca4a15fa7
Your Name <yadavdeepanshu000@gmail.com>
2025-02-09 23:46:10 +0530

    a

---
+++ Week 1/JS Basics/2.1-arrow-functions.js
@@ -0,0 +1,12 @@
+// From Apna College JS Playlist
+// Function
+function sum(a, b) {
+	return a + b;
+	}
+	let ans = sum(2, 3)
+	console.log(ans);
+	
+	// Arrow Function
+	const arrowSum = (c , d) => {
+			console.log(c + d);
+	}
