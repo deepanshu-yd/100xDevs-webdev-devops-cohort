afe14fcbc51301c0163deb21e9f8e9d6bf885084
deepanshu <yadavdeepanshu000@gmail.com>
2025-07-29 15:04:58 +0530

    middleware

--- Week 5/5.2-middleware/index.js
+++ Week 5/5.2-middleware/index.js
@@ -1,36 +1,31 @@
+// What are Middlewares?
+// middlewares are function that has access to request object (req), response object(res) and the next function in the application's request-response cycle
+// They can perform a variety of tasks: *Modifying* the request-response cycle, Ending the request-response cycle, and Calling the next middleware function in stack.
+
 const express = require ('express');
 const app = express();
 
 let requestCount = 0;
 
-function requestIncreaser() {
+function requestIncreaser(req, res, next) {
     requestCount = requestCount + 1;
+    req.name = "random123"; // using middleware to modify a request
     console.log(`Total number of Requests = ${requestCount}`);
+    next();
 }
 
 function realSumHandler(req, res) {
     const a = parseInt(req.query.a);
     const b = parseInt(req.query.b);
+    console.log(req.name);
 
     res.json({
         ans: a + b
     });
 }
 
-// route for addition
 app.get("/addition", requestIncreaser, realSumHandler);
 
-//route for multiplication
-app.get("/multiplication", function(req,res){
+// In this case, requestIncreaser is a Middleware, if only the process gets passed will the user will be able to access the realSumHandler
 
-    requestIncreaser();
-    // main logic
-    const a = req.query.a;
-    const b = req.query.b;
-
-    res.json({
-        ans: a * b
-    })
-});
-
-app.listen(3000)
+app.listen(3000);
