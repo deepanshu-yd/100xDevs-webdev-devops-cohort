9cc2273c14bf31e8b461c97137df95ad458aa557
deepanshu <yadavdeepanshu000@gmail.com>
2025-07-31 20:33:59 +0530

    auth assignment

---
+++ Week 6/express-app-assignment/index.js
@@ -0,0 +1,53 @@
+const express = require ('express');
+const app = express();
+app.use(express.json());
+
+const users = [];
+
+function generateToken(){
+    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+    let token = "";
+    for (let i = 0; i < 32; i++){
+        // using a simple function here
+        token+= options[Math.floor(Math.random() * options.length)]
+    }
+    return token;
+}
+
+app.post("/signup", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    users.push({
+        username: username,
+        password: password
+    })
+
+    res.send({
+        message: "You have been signed up!"
+    })
+});
+
+app.post("/signin", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const user = users.find(user.username === username && user.password === password);
+
+    if(user){
+        const token = generateToken();
+        user.token = token;
+        res.send({
+            token
+        })
+        console.log(users);
+    }
+    else{
+        res.status(403).send({
+            message: "Invalid username and password"
+        })
+    }
+});
+
+app.listen(3000);
