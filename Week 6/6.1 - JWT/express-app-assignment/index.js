aeb947662ad7735ab8bf3d6f304082b0359d778b
deepanshu <yadavdeepanshu000@gmail.com>
2025-08-01 10:19:47 +0530

    auth using jwt completed with assignment: self-solve left

--- Week 6/6.1 - JWT/express-app-assignment/index.js
+++ Week 6/6.1 - JWT/express-app-assignment/index.js
@@ -1,19 +1,17 @@
 const express = require ('express');
+const jwt = require("jsonwebtoken");
+
 const app = express();
+
 app.use(express.json());
+
+const JWT_SECRET = "deepanshuilovecomputers";
 
 const users = [];
 
-function generateToken(){
-    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// removed the function to create a random token, now using jwt
 
-    let token = "";
-    for (let i = 0; i < 32; i++){
-        // using a simple function here
-        token+= options[Math.floor(Math.random() * options.length)]
-    }
-    return token;
-}
+
 
 app.post("/signup", (req, res) => {
     const username = req.body.username;
@@ -36,9 +34,12 @@ app.post("/signin", (req, res) => {
     const user = users.find( user => user.username === username && user.password === password);
 
     if(user){
-        const token = generateToken();
-        user.token = token;
-        res.send({
+        const token = jwt.sign({
+            username: username
+        }, JWT_SECRET); // convert the username to a jwt
+
+        // user.token = token;
+        res.send({ //res.json
             token
         })
         console.log(users);
@@ -51,7 +52,10 @@ app.post("/signin", (req, res) => {
 
 app.get("/me", (req, res) => {
     const token = req.headers.authorization;
-    const user = users.find(user => user.token === token);
+    const decodedInformation = jwt.verify(token, JWT_SECRET);
+    const username = decodedInformation.username;
+
+    const user = users.find(user => user.username === username);
 
     if (user){
         res.send({
