0eba99ff1ff10f9b80de049a04f5abec937e1cfb
deepanshu <yadavdeepanshu000@gmail.com>
2025-08-07 20:28:43 +0530

    auth middleware assignment completed

--- Week 6/6.2 - Auth FE and BE/auth-middleware-assignment/index.js
+++ Week 6/6.2 - Auth FE and BE/auth-middleware-assignment/index.js
@@ -1,3 +1,7 @@
+// assignment: create a middleware that verifies if the user is logged in and will end the request early if the user is not logged in
+
+// my approach -> add a auth middleware that runs before the function to verify user once for all: p.s. same solution
+
 const express = require ('express');
 const jwt = require("jsonwebtoken");
 
@@ -46,10 +50,25 @@ app.post("/signin", (req, res) => {
     }
 });
 
-app.get("/me", (req, res) => {
+function auth(req, res, next){
     const token = req.headers.authorization;
     const decodedInformation = jwt.verify(token, JWT_SECRET);
-    const username = decodedInformation.username;
+    if (decodedInformation.username){
+        req.username = decodedInformation.username;
+        next();
+    } else {
+        res.json({
+            message: "You are not logged in"
+        })
+    }
+
+}
+
+app.get("/me", auth, (req, res) => {
+    // const token = req.headers.authorization;
+    // const decodedInformation = jwt.verify(token, JWT_SECRET); // commenting so that it would be included in the middleware that verifies just once, insted of doing it again and again for all the endpoints
+
+    const username = req.username; // Use req.username set by the auth middleware
 
     const user = users.find(user => user.username === username);
 
