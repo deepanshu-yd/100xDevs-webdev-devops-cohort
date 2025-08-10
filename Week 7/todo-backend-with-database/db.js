1f84905049d3c22a18908196cc9963728ed85cb0
deepanshu <yadavdeepanshu000@gmail.com>
2025-08-10 17:24:05 +0530

    fking confusing

--- Week 7/todo-backend-with-database/db.js
+++ Week 7/todo-backend-with-database/db.js
@@ -1,11 +1,24 @@
-const mongoose = require('mosgoose');
+const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
 const User = new Schema({
     name: String,
-    email: String,
+    email: {type: String, unique: true},
     password: String
 });
 
+const Todo = new Schema ({
+    userID: String,
+    title: String,
+    done: Boolean
+});
+
+const UserModel = mongoose.model('users', User);
+const TodoModel = mongoose.model('todos', Todo);
+
+module.exports = {
+    UserModel,
+    TodoModel
+}
--- Week 7/todo-backend-with-database/index.js
+++ Week 7/todo-backend-with-database/index.js
@@ -1,20 +1,57 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://yadavdeepanshu000:uFcAbL0AZVFCO5rvf@cluster0.n3uj5jq.mongodb.net/todo-app-database")
+
 const express = require('express');
 
 const app = express();
 app.use(express.json());
 
+const { userModel, TodoModel, UserModel } = require("./db");
+const JWT_SECRET = "deepanshucodes";
+
 // create the skeleton of 4 routes: POST/signup, POST/signin, POST/todo(authenticated), GET/todo(authenticated)
 
-app.post("/signup", function(req, res){
+app.post("/signup", async function(req, res){
+    const email = req.body.email;
+    const password = req.body.password;
+    const name = req.body.name;
 
+    await userModel.create({
+        email:email,
+        password:password,
+        name: name
+    });
+
+    res.json({
+        message:"You are signed up!"
+    })
 });
 
-app.post("/signin", function(req, res){
+app.post("/signin", async function(req, res){
+    const email = req.body.email;
+    const password = req.body.password;
 
+    const response = await UserModel.findOne({
+        email: email,
+        password: password
+    });
+
+    if (response) {
+        const token = jwt.sign({
+            id:response._id.toString()
+        })
+        res.json({
+            token
+        })
+    } else {
+        res.status(403).json({
+            message: "Incorrect creds"
+        })
+    }
 });
 
 app.post("/todo", function(req, res){
-
+    const { auth, JWT_SECRET } = require("./auth");
 });
 
 app.get("/todo", function (req, res){
