4035fe97c0c8c6884e861c31a1cf1a93285438a7
deepanshu <yadavdeepanshu000@gmail.com>
2025-08-30 20:49:16 +0530

    useState Hook

--- Week 9/src/App.jsx
+++ Week 9/src/App.jsx
@@ -13,9 +13,19 @@ function Counter () {
     setCount(count+1);
   }
 
+  function decreaseCount () {
+    setCount(count-1);
+  }
+
+  function resetCount () {
+    setCount(0);
+  }
+
   return <div>
     <h1>{count}</h1>
     <button onClick={increaseCount}>Increase Count</button>
+    <button onClick={decreaseCount}>Decrease Count</button>
+    <button onClick={resetCount}>Reset Count</button>
   </div>
 }
 
