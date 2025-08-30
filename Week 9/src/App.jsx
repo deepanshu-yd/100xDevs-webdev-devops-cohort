00518c9eb4c603ebf21fca80830e4f5b06773853
deepanshu <yadavdeepanshu000@gmail.com>
2025-08-30 17:11:54 +0530

    react lecture 2 start

---
+++ Week 9/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+function App (){
+  return <div>
+    <Counter></Counter>
+  </div>
+}
+
+function Counter () {
+  const [count, setCount] = useState(0);
+
+  function increaseCount (){
+    setCount(count+1);
+  }
+
+  return <div>
+    <h1>{count}</h1>
+    <button onClick={increaseCount}>Increase Count</button>
+  </div>
+}
+
+export default App;
