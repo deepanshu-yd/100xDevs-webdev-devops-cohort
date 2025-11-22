2c0b9f7efd03815df6cae3903002e530857a3b38
deepanshu <yadavdeepanshu000@gmail.com>
2025-11-22 13:31:22 +0530

    Conditional rendering - increase count

--- Week 9/week-9-react-offline/src/App.jsx
+++ Week 9/week-9-react-offline/src/App.jsx
@@ -1,23 +1,29 @@
-import { useState } from "react"
-function App () {
+import { useState } from "react";
+function App() {
   return (
-    <div style={{background: "#dfe6e9", height: "100vh"}}>
+    <div style={{ background: "#dfe6e9", height: "100vh" }}>
       <ToggleMessage />
       <ToggleMessage />
       <ToggleMessage />
     </div>
-  )
+  );
 }
 
 const ToggleMessage = () => {
-  const [isVisible, setVisible] = useState(false);
+  let [notificationCount, setNotificaitonCount] = useState(0);
+
+  console.log("re-render");
+
+  function increment() {
+    setNotificaitonCount(notificationCount + 1);
+  }
 
   return (
     <div>
-      <button onClick={ () => { setVisible (!isVisible)}}>
-        Toggle Message
-        </button>
-        {isVisible && <p>This Message is conditionally rendered!</p>}
+      <button onClick={increment}>
+        Increase count
+      </button>
+      {notificationCount}
     </div>
   );
 };
