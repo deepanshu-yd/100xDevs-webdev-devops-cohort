0767c1c9814c633c83eb01b0147b632b62038e2e
deepanshu <yadavdeepanshu000@gmail.com>
2025-11-22 15:02:22 +0530

    Finalll -> Rendeing component based on button click

--- Week 9/week-9-react-offline/src/App.jsx
+++ Week 9/week-9-react-offline/src/App.jsx
@@ -2,20 +2,32 @@ import { useState } from "react";
 import { PostComponent } from "./Post";
 
 function App() {
-  function addPost() {}
+  const [posts, setPosts] = useState([]);
+
+  const postComponents = posts.map(post => <PostComponent
+            name={post.name}
+            subtitle={post.subtitle}
+            time={post.time}
+            image={post.image}
+            description={post.description}
+          />)
+
+  function addPost() {
+    setPosts([...posts, {
+    name: "Deepanshu",
+    subtitle: "100 followers",
+    time: "22m ago",
+    image: "https://avatars.githubusercontent.com/u/93039894?s=400&u=e35e3041fbfc9e93e8ecb7015bbd67300f32ee4b&v=4",
+    description: "How to get a Job in 2025? Here's a set of all the resources and techniques you would want to know to prepare yourself for a job..."
+    }])
+  }
 
   return (
     <div style={{ background: "#dfe6e9", height: "100vh" }}>
       <button onClick={addPost}>Add Post</button>
       <div style={{ display: "flex", justifyContent: "center" }}>
         <div>
-          <PostComponent
-            name="Deepanshu"
-            subtitle="100 followers"
-            time="22m ago"
-            image={"https://avatars.githubusercontent.com/u/93039894?s=400&u=e35e3041fbfc9e93e8ecb7015bbd67300f32ee4b&v=4"}
-            description={"How to get a Job in 2025? Here's a set of all the resources and techniques you would want to know to prepare yourself for a job..."}
-          />
+          {postComponents}
         </div>
       </div>
     </div>
