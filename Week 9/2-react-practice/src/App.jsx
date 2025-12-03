ad2598a8d6d6c60e81c99f93ef2f4ca9a3aa3917
deepanshu <yadavdeepanshu000@gmail.com>
2025-12-03 17:43:32 +0530

    timer component

--- Week 9/2-react-practice/src/App.jsx
+++ Week 9/2-react-practice/src/App.jsx
@@ -1,36 +1,21 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [currentTab, setCurrentTab] = useState(1);
-  const [tabData, setTabData] = useState ({});
-  const [loading, setLoading] = useState(false);
-
-  useEffect(function() {
-    setLoading("true");
-    fetch("https://jsonplaceholder.typicode.com/todos/" + currentTab)
-      .then(async res => {
-      const json = await res.json();
-      setTabData(json)
-      setLoading(false)
-    });
-  }, [currentTab])
-
   return <div>
-    <button onClick={function() {
-      setCurrentTab(1);
-    }} style={{color: currentTab == 1 ? "red" : "black"}}>Todo #1</button>
-    <button onClick={function() {
-      setCurrentTab(2);
-    }} style={{color: currentTab == 2 ? "red" : "black"}}>Todo #2</button>
-    <button onClick={function() {
-      setCurrentTab(3)
-    }} style={{color: currentTab == 3 ? "red" : "black"}}>Todo #3</button>
-    <button onClick={function() {
-      setCurrentTab(4)
-    }} style={{color: currentTab == 4 ? "red" : "black"}}>Todo #4</button>
-<br></br>
-    {loading ? "Loading..." : tabData.title}
+    <Timer />
   </div>
+}
+
+const Timer = () => {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      setSeconds(prev => prev + 1);
+    }, 1000)
+  }, []);
+
+  return <div>{seconds} seconds elapsed</div>
 }
 
 export default App
